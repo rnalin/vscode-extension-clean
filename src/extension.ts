a7617ba321154252832a926ee09f1139ebc564e0
Renan Nalin <nalin.renan@gmail.com>
2022-08-23 13:53:56 -0300

    creating extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,76 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+import * as fs from "fs";
+import * as vscode from "vscode";
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prouserarch" is now active!');
+  console.log(
+    'Congratulations, your extension "ProuserApps Architecture" is now active!'
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('prouserarch.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from prouserarch!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "prouserarch.helloWorld",
+    async (uri: vscode.Uri) => {
+      const userFeatureName = await vscode.window.showInputBox({
+        prompt: "Feature name?",
+        placeHolder: "Ex: login or get_products",
+      });
 
-	context.subscriptions.push(disposable);
+      if (!userFeatureName) {
+        vscode.window.showErrorMessage("Feature name is required!");
+        return;
+      }
+
+      if (userFeatureName?.includes(" ")) {
+        vscode.window.showErrorMessage("Spaces are not allowed!");
+        return;
+      }
+
+      const customFolder = uri.fsPath;
+
+      let indexOfLibFolder = customFolder.indexOf("/lib", 0);
+      const rootFolder = customFolder.substring(0, indexOfLibFolder) + "/lib";
+
+      let folderList = vscode.workspace
+        .getConfiguration("scaffolding")
+        .get("layers.templates");
+
+      let makeTestFolder = vscode.workspace
+        .getConfiguration("scaffolding")
+        .get("layers.test");
+
+      if (folderList && Array.isArray(folderList)) {
+        try {
+          let featureFolder = "";
+
+          folderList.forEach((element) => {
+            featureFolder = element.replaceAll("{root_folder}", rootFolder);
+            featureFolder = featureFolder.replace(
+              "{custom_folder}",
+              customFolder
+            );
+            featureFolder = featureFolder.replace(
+              "{feature_name}",
+              userFeatureName
+            );
+
+            const testFeatureFolder = featureFolder.replace("/lib", "/test");
+
+            fs.mkdirSync(featureFolder, { recursive: true });
+
+            if (makeTestFolder) {
+              fs.mkdirSync(testFeatureFolder, { recursive: true });
+            }
+          });
+        } catch (err) {
+          console.log("Error", err);
+          throw err;
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
